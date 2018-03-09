20d617bec4c583a48b35cfa8ca81e90510fe9fc7
Rishabh Yadav <Rishabh_Yadav@ad.infosys.com>
2018-03-09 16:48:56 +0530

    adding mlabs database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,12 @@
 var mongoose = require('mongoose');
 
+//mongodb://ryada36:montiY#89@ds163418.mlab.com:63418/todo
 //to use promises with mongoose
+
+const REMOTE_MONGO='mongodb://ryada36:montiY#89@ds163418.mlab.com:63418/todo';
+
 mongoose.Promise=global.Promise;
-const DB_URL=process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const DB_URL=process.env.PORT ? REMOTE_MONGO : 'mongodb://localhost:27017/TodoApp';
 mongoose.connect(DB_URL);
 
 module.exports={mongoose}
